4ac48f1c2781221e7fd595969210bbf50893f83a
Besim Strazimiri <besimstrazimiri@outlook.fr>
2025-09-08 17:35:34 +0200

    commit #5

---
+++ www/js/qr.js
@@ -0,0 +1,44 @@
+var ipServeur = '172.17.50.131';     // Adresse ip du serveur  
+var ws;
+
+
+window.onload = function () {
+    if (TesterLaCompatibilite()) {
+        ConnexionAuServeurWebsocket();
+    }
+    ControleIHM();
+}
+function TesterLaCompatibilite() {
+    let estCompatible = true;
+    if (!('WebSocket' in window)) {
+        window.alert('WebSocket non supporté par le navigateur');
+        estCompatible = false;
+    }
+    return estCompatible;
+}
+/*  ***************** Connexion au serveur WebSocket ********************   */
+// 
+function ConnexionAuServeurWebsocket() {
+    ws = new WebSocket('ws://' + ipServeur + '/qr');
+
+    ws.onclose = function (evt) {
+        window.alert('WebSocket close');
+    };
+
+    ws.onopen = function () {
+        console.log('WebSocket open');
+    };
+
+    ws.onmessage = function (evt) {
+        document.getElementById('messageRecu').value = evt.data;
+    };
+}
+
+function ControleIHM() {
+    document.getElementById('Envoyer').onclick = BPEnvoyer;
+}
+
+function BPEnvoyer() {
+    ws.send(document.getElementById('messageEnvoi').value);
+}
+
