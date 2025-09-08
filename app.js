c29ebacfbf1b9bdb40cb555ed22b74a7c3668d15
Besim Strazimiri <besimstrazimiri@outlook.fr>
2025-09-08 15:44:34 +0200

    commit #2

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 'use strict';
 
+const port = 80;
+
+var express = require('express');
+
+var exp = express(); 
+
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+}); 
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+}); 
+
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+}); 
+
 console.log('TP CIEL');
